0c14fc3f78137a82369156b71abb39211f33ba2a
Honufa <devhonufa@gmail.com>
2022-05-17 15:04:13 +0600

    some text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.project-popup').magnificPopup({
-        type: 'image',
+        type: "image",
         gallery:{
-        enabled:true
-  }
-        
+        enabled:true,
+        },     
     });
+    // vdo js popup
+    $('.video').magnificPopup({
+        type: "iframe",     
+    });
+    $(function(){
+        $('#menu').slicknav();
+    });
+    // counter js code
+
+    $('.counter').counterUp();
+    $('.testimonials').slick({
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-regular fa-circle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-regular fa-circle-right"></i></button>',
+        dots: true,
+        autoplay: true,
+    });
+    // sticky header js
+
+    
+    
+
 });
